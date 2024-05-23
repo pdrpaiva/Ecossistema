8e0774ff11fd3682d7904c632ba26ed3d96c2779
tomasgferreira06 <102865501+tomasgferreira06@users.noreply.github.com>
2024-05-23 14:19:35 +0100

    update
    update

--- Ecossistema/Ecossistema/src/pt/isec/pa/javalife/model/data/fsm/states/MovimentoState.java
+++ Ecossistema/Ecossistema/src/pt/isec/pa/javalife/model/data/fsm/states/MovimentoState.java
@@ -1,9 +1,29 @@
 package pt.isec.pa.javalife.model.data.fsm.states;
 
+import pt.isec.pa.javalife.model.data.elements.Fauna;
+import pt.isec.pa.javalife.model.data.fsm.FaunaContext;
+import pt.isec.pa.javalife.model.data.fsm.FaunaState;
 import pt.isec.pa.javalife.model.data.fsm.FaunaStateAdapter;
 
 public class MovimentoState extends FaunaStateAdapter {
 
+    public MovimentoState(FaunaContext context, Fauna data) {
+        super(context, data);
+    }
 
 
+
+    @Override
+    public boolean procurarComida() {
+        if(data.getForca() < 35){
+            changeState(FaunaState.PROCURA_COMIDA);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public FaunaState getState(){
+        return FaunaState.MOVIMENTO;
+    }
 }
