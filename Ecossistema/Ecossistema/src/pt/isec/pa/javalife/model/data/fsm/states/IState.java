5a04def77933c1904beff3dc09e15d0b3ff608a4
Pedro Paiva <pedrorrvpaiva@gmail.com>
2024-05-31 17:49:33 +0100

    x

--- Ecossistema/Ecossistema/src/pt/isec/pa/javalife/model/data/fsm/states/IState.java
+++
@@ -1,7 +0,0 @@
-package pt.isec.pa.javalife.model.data.fsm.states;
-
-public interface IState {
-    void enterState();
-    void exitState();
-    void handleEvent(String event);
-}
--- Ecossistema/Ecossistema/src/pt/isec/pa/javalife/ui/gui/resources/ImageManager.java
+++
@@ -1,4 +0,0 @@
-package pt.isec.pa.javalife.ui.gui.resources;
-
-public class ImageManager {
-}
